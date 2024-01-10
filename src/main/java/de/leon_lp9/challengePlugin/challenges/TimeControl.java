51f787412e33e6b197de654bf2e8cef45b1ed364
Leonlp9 <leon.marcel.rabe@gmail.com>
2024-01-10 14:20:44 +0100

    Gamerule

---
+++ src/main/java/de/leon_lp9/challengePlugin/challenges/TimeControl.java
@@ -0,0 +1,113 @@
+package de.leon_lp9.challengePlugin.challenges;
+
+import de.leon_lp9.challengePlugin.Main;
+import de.leon_lp9.challengePlugin.builder.ItemBuilder;
+import de.leon_lp9.challengePlugin.challenges.config.LoadChallenge;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.ServerTickManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@LoadChallenge
+public class TimeControl extends Challenge{
+    private transient List<Player> cooldown = new ArrayList<>();
+    private ServerTickManager serverTickManager;
+
+    public TimeControl() {
+        super(Material.CLOCK);
+        serverTickManager = Bukkit.getServer().getServerTickManager();
+    }
+
+    @Override
+    public void unregister() {
+        super.unregister();
+        unfreeze();
+
+        //entferne alle Items mit dem Tag "timeControl"
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            for (ItemStack itemStack : player.getInventory()) {
+                if (itemStack != null && itemStack.hasItemMeta() && itemStack.getItemMeta().getPersistentDataContainer().has(new NamespacedKey(Main.getInstance(), "timeControl"), PersistentDataType.STRING)){
+                    if (itemStack.getItemMeta().getPersistentDataContainer().get(new NamespacedKey(Main.getInstance(), "timeControl"), PersistentDataType.STRING).equals("timeControlPause") || itemStack.getItemMeta().getPersistentDataContainer().get(new NamespacedKey(Main.getInstance(), "timeControl"), PersistentDataType.STRING).equals("timeControlResume")){
+                        player.getInventory().remove(itemStack);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void register() {
+        super.register();
+        unfreeze();
+        ItemStack timePause = new ItemBuilder(Material.CLOCK).setDisplayName("§6Time Control (Pause)").addPersistentDataContainer("timeControl", PersistentDataType.STRING, "timeControlPause").build();
+
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            player.getInventory().addItem(timePause);
+        });
+    }
+
+    public void freeze(){
+        serverTickManager.setFrozen(true);
+
+        //Items Tauschen von Pause zu Resume
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            for (ItemStack itemStack : player.getInventory()) {
+                if (itemStack != null && itemStack.hasItemMeta() && itemStack.getItemMeta().getPersistentDataContainer().has(new NamespacedKey(Main.getInstance(), "timeControl"), PersistentDataType.STRING)){
+                    if (itemStack.getItemMeta().getPersistentDataContainer().get(new NamespacedKey(Main.getInstance(), "timeControl"), PersistentDataType.STRING).equals("timeControlPause")){
+                        ItemStack timeResume = new ItemBuilder(Material.CLOCK).setDisplayName("§6Time Control (Resume)").addPersistentDataContainer("timeControl", PersistentDataType.STRING, "timeControlResume").build();
+                        itemStack.setItemMeta(timeResume.getItemMeta());
+                    }
+                }
+            }
+        });
+
+    }
+
+    public void unfreeze(){
+        serverTickManager.setFrozen(false);
+
+        //Items Tauschen von Resume zu Pause
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            for (ItemStack itemStack : player.getInventory()) {
+                if (itemStack != null && itemStack.hasItemMeta() && itemStack.getItemMeta().getPersistentDataContainer().has(new NamespacedKey(Main.getInstance(), "timeControl"), PersistentDataType.STRING)){
+                    if (itemStack.getItemMeta().getPersistentDataContainer().get(new NamespacedKey(Main.getInstance(), "timeControl"), PersistentDataType.STRING).equals("timeControlResume")){
+                        ItemStack timePause = new ItemBuilder(Material.CLOCK).setDisplayName("§6Time Control (Pause)").addPersistentDataContainer("timeControl", PersistentDataType.STRING, "timeControlPause").build();
+                        itemStack.setItemMeta(timePause.getItemMeta());
+                    }
+                }
+            }
+        });
+
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e){
+        if (cooldown.contains(e.getPlayer())){
+            return;
+        }
+        if (e.getItem() != null && e.getItem().getType().equals(Material.CLOCK)){
+            if (serverTickManager.isFrozen()){
+                unfreeze();
+                cooldown.add(e.getPlayer());
+                Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> {
+                    cooldown.remove(e.getPlayer());
+                }, 10);
+            }else{
+                freeze();
+                cooldown.add(e.getPlayer());
+                Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> {
+                    cooldown.remove(e.getPlayer());
+                }, 10);
+            }
+        }
+    }
+
+}
