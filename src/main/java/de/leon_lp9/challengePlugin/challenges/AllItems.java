269be47ba608910c19d29a5fc7b6a7ff5202f3a9
Leonlp9 <leon.marcel.rabe@gmail.com>
2024-01-12 11:07:22 +0100

    Challenges

---
+++ src/main/java/de/leon_lp9/challengePlugin/challenges/AllItems.java
@@ -0,0 +1,29 @@
+package de.leon_lp9.challengePlugin.challenges;
+
+import de.leon_lp9.challengePlugin.challenges.config.LoadChallenge;
+import org.bukkit.Material;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@LoadChallenge
+public class AllItems extends Challenge {
+
+    private ArrayList<Material> allMaterialsRandomized;
+    private ArrayList<Material> allCollectedItems;
+
+    public AllItems() {
+        super(Material.ENDER_CHEST);
+        allCollectedItems = new ArrayList<>();
+
+        allMaterialsRandomized = (ArrayList<Material>) Arrays.stream(Material.values()).toList();
+        allMaterialsRandomized.removeIf(material -> material.isAir() || material.isLegacy());
+        //shuffel the list
+        Collections.shuffle(allMaterialsRandomized);
+    }
+
+
+
+}
--- src/main/java/de/leon_lp9/challengePlugin/gamerules/SyncInventory.java
+++ src/main/java/de/leon_lp9/challengePlugin/gamerules/SyncInventory.java
@@ -1,9 +1,14 @@
 package de.leon_lp9.challengePlugin.gamerules;
 
 import de.leon_lp9.challengePlugin.gamerules.config.LoadGamerule;
+import io.papermc.paper.event.player.PlayerPickItemEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 @LoadGamerule
 public class SyncInventory extends GameRule {
@@ -13,7 +18,31 @@ public class SyncInventory extends GameRule {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
+        syncInventory((Player) event.getWhoClicked());
+    }
 
+    @EventHandler
+    public void onItemPickUp(PlayerPickItemEvent event) {
+        syncInventory(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event) {
+        syncInventory(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onItemDrop(PlayerInteractEvent event) {
+        syncInventory(event.getPlayer());
+    }
+
+    public void syncInventory(Player ofPlayer){
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            if(onlinePlayer.getUniqueId().equals(ofPlayer.getUniqueId())) continue;
+            onlinePlayer.getInventory().setContents(ofPlayer.getInventory().getContents());
+            onlinePlayer.getInventory().setArmorContents(ofPlayer.getInventory().getArmorContents());
+            onlinePlayer.updateInventory();
+        }
     }
 
 }
