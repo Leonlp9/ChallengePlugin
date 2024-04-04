abc8a684e9cbb5010a006067555b8460aad10795
Leon_lp9 <73760817+Leonlp9@users.noreply.github.com>
2024-04-05 00:27:03 +0200

    Update

--- docs/javascript.js
+++ docs/javascript.js
@@ -2,19 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
     var nav = document.querySelector('nav');
     if (nav) {
         nav.innerHTML = `
-            <img src="images/logo.png" style="width: calc(100% - 20px); margin: 15px 10px 10px;">
-            <a href="index.html" class="mt-4">Home</a>
-            <div class="navbar-label">Features</div>
-            <ul>
-                <li><a href="challenges.html">Challenges</a></li>
-                <li><a href="showcase.html">Showcase</a></li>
-            </ul>
-
-            <div class="navbar-label">Use</div>
-            <ul>
-                <li><a href="install.html">Installation</a></li>
-                <li><a href="api.html">API</a></li>
-            </ul>
+            <div>
+                <img src="images/logo.png" style="width: calc(100% - 20px); margin: 15px 10px 10px;">
+                <a href="index.html" class="mt-4">Home</a>
+                <div class="navbar-label">Features</div>
+                <ul>
+                    <li><a href="challenges.html">Challenges</a></li>
+                    <li><a href="showcase.html">Showcase</a></li>
+                </ul>
+    
+                <div class="navbar-label">Use</div>
+                <ul>
+                    <li><a href="install.html">Installation</a></li>
+                    <li><a href="api.html">API</a></li>
+                </ul>
+            </div>
+            <div class="buttonMenu">
+                <button><i class="fa-solid fa-sun"></i></button>
+                <button><i class="fa-solid fa-angles-left"></i></button>
+            </div>
         `;
     }
 });
